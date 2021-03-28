708de9f2277d88fce87818850ffc2c393914f102
Lucas Bottrel Lopes de Moura <65674658+lucasbottrel@users.noreply.github.com>
2021-03-28 17:40:33 -0300

    Adicionado numero de pessoas que bebem e que não bebem

--- lib/main.dart
+++ lib/main.dart
@@ -27,16 +27,17 @@ class _HomePageState extends State<HomePage> {
 
   // VARIAVEIS
   final _valorConta = TextEditingController();
-  final _quantPessoas = TextEditingController();
-  double _currentSliderValue = 20;
-  var _infoText = "Informe seus dados!";
+  final _quantPessoasBebem = TextEditingController();
+  final _quantPessoasNaoBebem = TextEditingController();
+  double _currentSliderValue = 0;
+  var _infoText = "";
   var _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Racha Conta"),
+        title: Text("RACHA CONTA"),
         centerTitle: true,
         actions: <Widget>[
           IconButton(icon: Icon(Icons.refresh),
@@ -50,9 +51,10 @@ class _HomePageState extends State<HomePage> {
   // PROCEDIMENTO PARA LIMPAR OS CAMPOS
   void _resetFields(){
     _valorConta.text = "";
-    _quantPessoas.text="";
+    _quantPessoasBebem.text="";
+    _quantPessoasNaoBebem.text="";
     setState(() {
-      _infoText = "Informe a temperatura!";
+      _infoText = "";
       _formKey = GlobalKey<FormState>();
     });
   }
@@ -66,17 +68,17 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               _editText("Digite o valor da conta: ", _valorConta),
-              _editText("Digite o número de pessoas: ", _quantPessoas),
+              _editText("Digite o número de pessoas que bebem: ", _quantPessoasBebem),
+              _editText("Digite o número de pessoas que não bebem: ", _quantPessoasNaoBebem),
               Text("\nPorcentagem do garçon", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15), ),
               Slider(value: _currentSliderValue,
                      min: 0,
-                     max: 100,
-                     divisions: 20,
+                     max: 40,
+                     divisions: 8,
                      label: _currentSliderValue.round().toString(),
                      onChanged: (double value){
                       setState(() {
                         _currentSliderValue=value.roundToDouble();
-                        print(_currentSliderValue);
                       });
                   }
               ),
@@ -94,13 +96,13 @@ class _HomePageState extends State<HomePage> {
       validator: (s) => _validate(s, field),
       keyboardType: TextInputType.number,
       style: TextStyle(
-        fontSize: 20,
+        fontSize: 18,
         color: Colors.indigo,
       ),
       decoration: InputDecoration(
         labelText: field,
         labelStyle: TextStyle(
-          fontSize: 22,
+          fontSize: 15,
           color: Colors.grey,
         ),
       ),
@@ -110,7 +112,7 @@ class _HomePageState extends State<HomePage> {
   // PROCEDIMENTO PARA VALIDAR OS CAMPOS
   String _validate(String text, String field) {
     if (text.isEmpty) {
-      return "Digite $field";
+      return "$field";
     }
     return null;
   }
@@ -144,11 +146,12 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       double _valorGarcom = ( _currentSliderValue / 100) * double.parse( _valorConta.text);
       double _valorTotal = double.parse( _valorConta.text) + _valorGarcom;
-      double _valorIndividual = _valorTotal / double.parse( _quantPessoas.text);
+      double _valorIndividualBebem = _valorTotal / double.parse( _quantPessoasBebem.text);
 
       String res = "Valor Garçom :                                        R\$ " + _valorGarcom.toStringAsPrecision(4) +
                    "\n\nValor Total:                                              R\$ " + _valorTotal.toStringAsPrecision(4) +
-                   "\n\nValor Individual:                                     R\$ " + _valorIndividual.toStringAsPrecision(4);
+                   "\n\nValor Individual (bebem):                         R\$ " + _valorIndividualBebem.toStringAsPrecision(4) +
+                   "\n\nValor Individual (não bebem):                     R\$ " + _valorIndividualBebem.toStringAsPrecision(4);
 
       _infoText = res;
     });
