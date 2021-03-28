40535325a30269cc7f2e425b382301b73042f478
Lucas Bottrel Lopes de Moura <65674658+lucasbottrel@users.noreply.github.com>
2021-03-28 19:32:55 -0300

    Liberação do APK

---
+++ build/app/generated/source/buildConfig/release/pucminas/br/racha_conta/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pucminas.br.racha_conta;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "pucminas.br.racha_conta";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
