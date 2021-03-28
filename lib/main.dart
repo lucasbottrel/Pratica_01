e6a1927499dccfa0c04e95cc26ab1970b77e9592
Lucas Bottrel Lopes de Moura <65674658+lucasbottrel@users.noreply.github.com>
2021-03-28 16:54:21 -0300

    Mudança de titulo

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,165 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Racha Conta',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // VARIAVEIS
+  final _valorConta = TextEditingController();
+  final _quantPessoas = TextEditingController();
+  double _currentSliderValue = 20;
+  var _infoText = "Informe seus dados!";
+  var _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Racha Conta"),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh),
+              onPressed: _resetFields)
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: _body(),
+    );
+  }
+
+  // PROCEDIMENTO PARA LIMPAR OS CAMPOS
+  void _resetFields(){
+    _valorConta.text = "";
+    _quantPessoas.text="";
+    setState(() {
+      _infoText = "Informe a temperatura!";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  _body() {
+    return SingleChildScrollView(
+        padding: EdgeInsets.all(15.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _editText("Digite o valor da conta: ", _valorConta),
+              _editText("Digite o número de pessoas: ", _quantPessoas),
+              Text("\nPorcentagem do garçon", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15), ),
+              Slider(value: _currentSliderValue,
+                     min: 0,
+                     max: 100,
+                     divisions: 20,
+                     label: _currentSliderValue.round().toString(),
+                     onChanged: (double value){
+                      setState(() {
+                        _currentSliderValue=value.roundToDouble();
+                        print(_currentSliderValue);
+                      });
+                  }
+              ),
+              _buttonCalcular(),
+              _textInfo(),
+            ],
+          ),
+        ));
+  }
+
+  // Widget text
+  _editText(String field, TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      validator: (s) => _validate(s, field),
+      keyboardType: TextInputType.number,
+      style: TextStyle(
+        fontSize: 20,
+        color: Colors.indigo,
+      ),
+      decoration: InputDecoration(
+        labelText: field,
+        labelStyle: TextStyle(
+          fontSize: 22,
+          color: Colors.grey,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  // PROCEDIMENTO PARA VALIDAR OS CAMPOS
+  String _validate(String text, String field) {
+    if (text.isEmpty) {
+      return "Digite $field";
+    }
+    return null;
+  }
+
+  // Widget button
+  _buttonCalcular() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0, bottom: 20),
+      height: 45,
+      child: RaisedButton(
+        color: Colors.indigo,
+        child:
+        Text(
+          "CALCULAR TOTAL A PAGAR",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 17,
+          ),
+        ),
+        onPressed: () {
+          if(_formKey.currentState.validate()){
+            _calculate();
+          }
+        },
+      ),
+    );
+  }
+
+  // PROCEDIMENTO PARA CALCULAR O IMC
+  void _calculate(){
+    setState(() {
+      double _valorGarcom = ( _currentSliderValue / 100) * double.parse( _valorConta.text);
+      double _valorTotal = double.parse( _valorConta.text) + _valorGarcom;
+      double _valorIndividual = double.parse( _valorConta.text) / double.parse( _quantPessoas.text);
+
+      String res = "Valor Garçom :                                        R\$ " + _valorGarcom.toStringAsPrecision(4) +
+                   "\n\nValor Total:                                              R\$ " + _valorTotal.toStringAsPrecision(4) +
+                   "\n\nValor Individual:                                     R\$ " + _valorIndividual.toStringAsPrecision(4);
+
+      _infoText = res;
+    });
+  }
+
+  // // Widget text
+  _textInfo() {
+    return Text(
+      _infoText,
+      textAlign: TextAlign.justify,
+      style: TextStyle(color: Colors.black, fontSize: 15.0, fontWeight: FontWeight.bold),
     );
   }
 }
